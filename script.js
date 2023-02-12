2b0e5b67fd60573560f7d93ac7915b0da05704bf
David Kapaku <dkapaku1@vt.edu>
2023-02-12 16:37:50 -0500

    first commit

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-let squareElement=document.getElementById("square")
-squareElement.setAttribute('square', squareElement.getAttribute('square'))
-changesquare.addEventListener("click", changeXO)
-function changeXO(clickedSquare){
-    let target=clickedSquare.target
-    if (target.className.length){
-        target.className =''
-    }
-    else {
-        target.className= x
-    }
-}
-let x = 'x'
-let o = 'o'
+//Gameplan: Give elements to each part that needs to be pressed
+//Player should alternate between X' and O'
+// Button click RESET should reset the gamestate to base
+//Win conditions 
+let winCon=[
+    [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]
+];
+let buttonElement=document.getElementById("reset")
+let messageElement=document.getElementsByTagName("p")
+let gameBoard= ['','','','','','','','','']
+let tableElement=document.getElementsByClassName("square")
+function clickOnSquare(event){
+    if (player1==true){
+        player1=false;
+    event.target.innerHTML = "X"
+    console.log(event.target)}
+    else if (player1==false) {
+        player1=true
+        event.target.innerHTML = "O"}
 
+}
+for(let i=0; i<tableElement.length; i++){tableElement[i].addEventListener("click", clickOnSquare)}
+let player1=true
+
+function gameRestart(){
+    gameBoard= ['','','','','','','','','']
+    gameboard.ForEach(tableElement)
+    }
+buttonElement.addEventListener("click", gameRestart)
